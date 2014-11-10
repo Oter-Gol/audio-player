07415534dee3471fffbf6aab2635600547e510be
oter <maxymhammer@yandex.ru>
2014-11-10 20:49:35 +0200

    Added main form

---
+++ MainForm/src/MainForm.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+/**
+ * Created by OTER on 11/10/2014.
+ */
+public class MainForm {
+    private JPanel panel1;
+    private JButton button1;
+
+    public static void Main() {
+
+    }
+}

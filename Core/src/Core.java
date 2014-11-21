9b72aa8b0af3ad0a9dec100a0a19de897c890fb9
oter <maxymhammer@yandex.ru>
2014-11-21 18:45:35 +0200

    Moved DEFAULT_BUFFER_SIZE constant value, implemented state machine (PLAY, PAUSE, STOP), improved sound output through SourceDataLine, added thread for this purposes. Added setter and getter for bufferSize. Implemented playPause method.

--- Core/src/Core.java
+++ Core/src/Core.java
@@ -1,8 +1,6 @@
 import javax.sound.sampled.*;
-import java.io.ByteArrayInputStream;
+import javax.swing.text.PlainDocument;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
 import java.util.Timer;
 
 /**
@@ -10,114 +8,149 @@ import java.util.Timer;
  */
 public class Core {
 
+    /*
+     * Default buffer size for play back audio
+     */
+    private final static int DEFAULT_BUFFER_SIZE = 4096;
+
+    /*
+     * Buffer size
+     */
+    private int bufferSize = DEFAULT_BUFFER_SIZE;
+
     private GlobalLoader globalLoader = new GlobalLoader();
 
     private Timer timer;
 
     private AudioFormat audioFormat;
 
+    public static enum PlayingStates { PLAY, PAUSE, STOP };
 
-    Clip clip;
+    private PlayingStates playingState = PlayingStates.STOP;
+
+    public PlayingStates getPlayingState() {
+        return playingState;
+    }
+
+    private class PlayingThread implements Runnable {
+
+        int timeSeek;
+
+        public int playPause() {
+
+            // To-Do perform check for valid format
+
+            // States for state machine
+            if ( playingState == PlayingStates.STOP ) {
+                playingState = PlayingStates.PLAY;
+
+                run();
+
+            } else {
+                if ( playingState == PlayingStates.PLAY ) {
+                    playingState = PlayingStates.PAUSE;
+
+                } else {
+                    playingState = PlayingStates.PLAY;
+                }
+            }
+
+            return 0;
+        }
+
+        public int seek( int timeSeek ) { return 0; }
+
+        public int stop() {
+            return 0;
+        }
+
+        /**
+         * When an object implementing interface <code>Runnable</code> is used
+         * to create a thread, starting the thread causes the object's
+         * <code>run</code> method to be called in that separately executing
+         * thread.
+         * <p/>
+         * The general contract of the method <code>run</code> is that it may
+         * take any action whatsoever.
+         *
+         * @see Thread#run()
+         */
+        @Override
+        public void run() {
+
+            SourceDataLine auline = null;
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+
+            try {
+                auline = (SourceDataLine) AudioSystem.getLine(info);
+                auline.open(audioFormat);
+            } catch (LineUnavailableException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            auline.start();
+
+            int nBytesRead = 0;
+
+            byte[] samplesRead = new byte[ bufferSize ];
+
+            nBytesRead = globalLoader.readSampledBytes( bufferSize, samplesRead );
+
+            globalLoader.setCurrentOffset( 0 );
+
+            while (nBytesRead != 0) {
+                auline.write(samplesRead, 0, nBytesRead );
+                nBytesRead = globalLoader.readSampledBytes( bufferSize, samplesRead );
+            }
+
+            auline.drain();
+            auline.close();
+
+        }
+    }
+
+   public PlayingThread playingThread;
+
 
     public int open( String filePath ) {
 
         globalLoader.load( filePath );
 
         if ( globalLoader.isValid() ) {
-            audioFormat = new AudioFormat( globalLoader.getSampleRate(), globalLoader.getSampleSizeInBits(),
-                    globalLoader.getChannels(), true, false );
+            audioFormat = new AudioFormat(globalLoader.getSampleRate(), globalLoader.getSampleSizeInBits(),
+                    globalLoader.getChannels(), true, false);
         }
 
-
-
-
-        byte [] samplesBateArray = new byte[ globalLoader.getDataLength() ];
-
-        ByteArrayInputStream samplesStream = new ByteArrayInputStream( samplesBateArray );
-
-        AudioInputStream audioInputStream = new AudioInputStream( samplesStream, audioFormat, globalLoader.getDataLength() );
-
-        SourceDataLine auline = null;
-        DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
-
-        try {
-            auline = (SourceDataLine) AudioSystem.getLine(info);
-            auline.open(audioFormat);
-        } catch (LineUnavailableException e) {
-            e.printStackTrace();
-            return -1;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return -1;
-        }
-
-//        if (auline.isControlSupported(FloatControl.Type.PAN)) {
-//            FloatControl pan = (FloatControl) auline
-//                    .getControl(FloatControl.Type.PAN);
-//            if (curPosition == Position.RIGHT)
-//                pan.setValue(1.0f);
-//            else if (curPosition == Position.LEFT)
-//                pan.setValue(-1.0f);
-//        }
-
-        auline.start();
-        int nBytesRead = 0;
-        byte[] abData = new byte[20000];
-
-        try {
-            while (nBytesRead != -1) {
-                nBytesRead = audioInputStream.read(abData, 0, abData.length);
-                if (nBytesRead >= 0)
-                    auline.write(abData, 0, nBytesRead);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return -1;
-        } finally {
-            auline.drain();
-            auline.close();
-        }
-
-
-
-
-
-//        byte sign = 1;
-//        while (frame.isVisible()) {
-//            int threshold = audioFormat.getFrameRate() / sliderValue;
-//            for (int i = 0; i < bufferSize; i++) {
-//                if (counter > threshold) {
-//                    sign = (byte) -sign;
-//                    counter = 0;
-//                }
-//                buffer[i] = (byte) (sign * 30);
-//                counter++;
-//            }
-//            // the next call is blocking until the entire buffer is
-//            // sent to the SourceDataLine
-//
-//        }
-
-
-
-
-
-
-
-
+        playingThread = new PlayingThread();
 
         return 0;
     }
 
-    public int play() {
-        return 0;
+
+
+
+    /**
+     * sets buffer size for the file. If not set,
+     * buffer size = DEFAULT_BUFFER_SIZE
+     * @param bufferSize
+     */
+    public void setBufferSize( int bufferSize ){
+        this.bufferSize = bufferSize;
     }
 
-    public int pause() {
-        return 0;
+    public int getBufferSize() {
+        return bufferSize;
     }
 
-    public int seek() { return 0; }
+
+    public int playPause() {
+
+        return playingThread.playPause();
+    }
+
+    public int seek( int timeSeek ) { return 0; }
 
     public int stop() {
         return 0;

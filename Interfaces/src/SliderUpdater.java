77e325fb89c76f8c70f987fb2ce17ee188752f50
oleh <olehgol260@gmail.com>
2014-11-22 18:24:57 +0200

    add interface which is necessary in seeking

---
+++ Interfaces/src/SliderUpdater.java
@@ -0,0 +1,12 @@
+/**
+ * Created by oleh on 22.11.14.
+ */
+public interface SliderUpdater {
+    /**
+     * TODO description
+     *
+     * @param value can be from 0 to 100
+     */
+    void updateSlider( float value );
+
+}
